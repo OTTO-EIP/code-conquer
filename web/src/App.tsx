6effb33b21aabc7c32e906eaf1970087115de3d3
Lenny Delgado <lenny.delgado@epitech.eu>
2024-07-02 11:21:37 +0200

    feat: init web stack

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="w-[2000px] h-[1000px] bg-black flex justify-center items-center">
+        <a className="w-20 h-12 bg-purple-700 rounded-lg" href="/">CACA</a>
+      </div>
+    </>
+  )
+}
+
+export default App
