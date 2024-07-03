84995ca14af617be5a0e31092bff4138c7dfcc91
Tbz <thibaut.tran@epitech.eu>
2024-07-03 17:19:11 +0200

    [ADD] Animated caracter & random walk

--- game/main.cpp
+++ game/main.cpp
@@ -1,11 +1,13 @@
-#include "raylib.cpp"
+#include "raylib.hpp"
 
 int main(int ac, char **av)
 {
     Raylib raylib;
+    std::srand(std::time(nullptr));
+
     while (!WindowShouldClose()) {
         raylib.testLandingPage();
     }
-    CloseWindow(); 
+    CloseWindow();
     return 0;
 }
--- game/raylib.cpp
+++ game/raylib.cpp
@@ -5,6 +5,9 @@ Raylib::Raylib()
     _windowName = "Code&Conquer";
     _windowSize = { 1920, 1080 };
     initWindow();
+
+    // Init character 1
+    _caracter = new Caracter("fox1.png", 12);
 }
 
 Raylib::~Raylib() {}
@@ -17,16 +20,38 @@ void Raylib::initWindow()
 
 void Raylib::testLandingPage()
 {
-    const char *text = "Hello, Welcome to Code & Conquer!";
-    int fontSize = 20;
-    int textWidth = MeasureText(text, fontSize);
-    int screenWidth = std::get<0>(_windowSize);
-    int screenHeight = std::get<1>(_windowSize);
-    int x = (screenWidth - textWidth) / 2;
-    int y = (screenHeight - fontSize) / 2;
+    // Mouvement time
+     float deltaTime = GetFrameTime();
+    _caracter->_elapsedTime += deltaTime;
 
+    // Update
+    if (_caracter->_elapsedTime >= _caracter->_changeDirectionTime) {
+        _caracter->_elapsedTime = 0.0f;
+        _caracter->_direction = std::rand() % 4;
+        switch (_caracter->_direction) {
+            case 0:
+                _caracter->_scarfy = &_caracter->_scarfyURight;
+                break;
+            case 1:
+                _caracter->_scarfy = &_caracter->_scarfyDRight;
+                break;
+            case 2:
+                _caracter->_scarfy = &_caracter->_scarfyDLeft;
+                break;
+            case 3:
+                _caracter->_scarfy = &_caracter->_scarfyULeft;
+                break;
+        }
+    }
+    _caracter->updateAnimation(18, 12, _caracter->_direction);
+    _caracter->mouvement();
+
+    // Draw
     BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText(text, x, y, fontSize, LIGHTGRAY);
+        ClearBackground(RAYWHITE);
+
+        // Draw character 1
+        DrawTextureRec(*_caracter->_scarfy, _caracter->_frameRec, _caracter->_position, WHITE);
+
     EndDrawing();
 }
