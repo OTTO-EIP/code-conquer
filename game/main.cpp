a033b9a17a6c32944a95893ad071cfc06935dd83
Zyad <zyad.othman@epitech.eu>
2024-07-04 15:33:00 +0200

    rebasing

--- game/main.cpp
+++ game/main.cpp
@@ -1,6 +1,8 @@
-#include "raylib/raylib.hpp"
+#include "src/Map/Map.hpp"
 #include "src/RayCam/RayCam.hpp"
+#include "src/Character/Character.hpp"
 #include <filesystem>
+#include <vector>
 
 #define PLAYER_SIZE 40
 
@@ -10,53 +12,62 @@ int main(int ac, char **av)
     Rectangle scene = { 0, 0, 1920, 1080 };
     RayCam camera(raylib.getWindowSize(), scene);
     Rectangle ScreenRect = { 0.0f, 0.0f, (float)camera.getScreenCamera().texture.width, (float)-camera.getScreenCamera().texture.height };
-    Texture2D textureGround = LoadTexture("assets/map/Tiles/grass_center_E.png");
+    Map map;
+    Entity ground_template("assets/map/Tiles/grass_center_E.png", {0, 0});
+    map.generateGround(ground_template, raylib);
+    Character *_caracter = new Character("fox1.png", 12);
 
     SetTargetFPS(60);
-    
 
     while (!WindowShouldClose()) {
+
+    float deltaTime = GetFrameTime();
+    _caracter->_elapsedTime += deltaTime;
+
+    if (_caracter->_elapsedTime >= _caracter->_changeDirectionTime) {
+        _caracter->_elapsedTime = 0.0f;
+        _caracter->_direction = std::rand() % 4;
+        switch (_caracter->_direction) {
+            case 0:
+                _caracter->_scarfy = &_caracter->_scarfyURight;
+                break;
+            case 1:
+                _caracter->_scarfy = &_caracter->_scarfyDRight;
+                break;
+            case 2:
+                _caracter->_scarfy = &_caracter->_scarfyDLeft;
+                break;
+            case 3:
+                _caracter->_scarfy = &_caracter->_scarfyULeft;
+                break;
+        }
+    }
+    _caracter->updateAnimation(18, 12, _caracter->_direction);
+    _caracter->mouvement();
+
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT) || IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+            Vector2 mouseDelta = GetMouseDelta();
+            camera._camera.target.x -= mouseDelta.x;
+            camera._camera.target.y -= mouseDelta.y;
+        }
         BeginTextureMode(camera.getScreenCamera());
             ClearBackground(RAYWHITE);
             
             BeginMode2D(camera.getCamera());
-            
-                // // Draw full scene with first camera
-                // for (int i = 0; i < screenWidth/PLAYER_SIZE + 1; i++)
-                // {
-                //     DrawLineV((Vector2){(float)PLAYER_SIZE*i, 0}, (Vector2){ (float)PLAYER_SIZE*i, (float)screenHeight}, LIGHTGRAY);
-                // }
-
-                // for (int i = 0; i < screenHeight/PLAYER_SIZE + 1; i++)
-                // {
-                //     DrawLineV((Vector2){0, (float)PLAYER_SIZE*i}, (Vector2){ (float)screenWidth, (float)PLAYER_SIZE*i}, LIGHTGRAY);
-                // }
-
-                // for (int i = 0; i < screenWidth/PLAYER_SIZE; i++)
-                // {
-                //     for (int j = 0; j < screenHeight/PLAYER_SIZE; j++)
-                //     {
-                //         DrawText(TextFormat("[%i,%i]", i, j), 10 + PLAYER_SIZE*i, 15 + PLAYER_SIZE*j, 10, LIGHTGRAY);
-                //     }
-                // }
-                // DrawRectangleRec(scene, RED);
+                map.draw();
+                DrawTextureRec(*_caracter->_scarfy, _caracter->_frameRec, _caracter->_position, WHITE);
             EndMode2D();
             
             
         EndTextureMode();
 
         BeginDrawing();
-            ClearBackground(BLACK);
-            
+            ClearBackground(BLACK);  
             DrawTextureRec(camera.getScreenCamera().texture, ScreenRect, (Vector2){ 0, 0 }, WHITE);
-            
-            // DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), LIGHTGRAY);
-            DrawRectangle(0, 0, GetScreenWidth(), 30, Fade(RAYWHITE, 0.6f));
-            DrawText("Welcome", 10, 10, 10, MAROON);
-            DrawTexture(textureGround, 300, 400, WHITE);
         EndDrawing();
     }
-
+    
     CloseWindow();
+    delete _caracter;
     return 0;
 }
--- game/run.sh
+++
@@ -1,1 +0,0 @@
-g++ main.cpp raylib/raylib.cpp -lm -lraylib -o run
