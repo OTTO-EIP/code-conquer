889cb89b974d629c404c4b26c91d4e8f8bb21103
Lenny Delgado <lenny.delgado@epitech.eu>
2024-07-05 12:03:01 +0200

    fix: code editor loose focus

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -12,11 +12,12 @@ const Dashboard: React.FC = () => {
   const [selectedTab, setSelectedTab] = useState('code');
   const [editorContent, setEditorContent] = useState('// Start coding...\n');
   const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
 
   const renderContent = () => {
     switch (selectedTab) {
       case 'code':
-        return <div id="editor-container" className="h-full w-full opacity-100"></div>;
+        return <div id="editor-container" ref={containerRef} className="h-full w-full opacity-100"></div>;
       case 'character':
         return <CharacterDisplay />;
       case 'script':
@@ -30,11 +31,10 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     const loaderUrl = 'https://cdn.jsdelivr.net/npm/monaco-editor@latest/min/vs/loader.js';
-    const editorUrl = 'https://cdn.jsdelivr.net/npm/monaco-editor@latest/min/vs/editor/editor.main.nls.js';
 
     const initEditor = () => {
-      if (!editorRef.current) {
-        const editorInstance = monaco.editor.create(document.getElementById('editor-container')!, {
+      if (containerRef.current && !editorRef.current) {
+        const editorInstance = monaco.editor.create(containerRef.current, {
           value: editorContent,
           theme: 'vs',
           minimap: { enabled: false },
@@ -52,13 +52,7 @@ const Dashboard: React.FC = () => {
     const loaderScript = document.createElement('script');
     loaderScript.src = loaderUrl;
     loaderScript.async = true;
-    loaderScript.onload = () => {
-      const editorScript = document.createElement('script');
-      editorScript.src = editorUrl;
-      editorScript.async = true;
-      editorScript.onload = initEditor;
-      document.body.appendChild(editorScript);
-    };
+    loaderScript.onload = initEditor;
     document.body.appendChild(loaderScript);
 
     return () => {
@@ -68,72 +62,53 @@ const Dashboard: React.FC = () => {
       }
       loaderScript.remove();
     };
-  });
+  }, []);
 
   useEffect(() => {
-    if (selectedTab === 'code') {
-      if (!editorRef.current) {
-        const editorInstance = monaco.editor.create(document.getElementById('editor-container')!, {
-          value: editorContent,
-          theme: 'vs',
-          minimap: { enabled: false },
-          language: 'javascript',
-        });
-
-        editorInstance.onDidChangeModelContent(() => {
-          setEditorContent(editorInstance.getValue());
-        });
-
-        editorRef.current = editorInstance;
-      }
-    } else {
-      if (editorRef.current) {
-        setEditorContent(editorRef.current.getValue());
-        editorRef.current.dispose();
-        editorRef.current = null;
-      }
+    if (selectedTab === 'code' && editorRef.current) {
+      editorRef.current.layout();
     }
-  }, [editorContent, selectedTab]);
+  }, [selectedTab]);
 
   return (
-    <div className="flex flex-col w-full h-full pt-20 space-y-6">
-      <div className="flex items-center justify-between w-full h-[90%]">
-        <div className="w-8/12 h-full bg-transparent border-4 border-green opacity-90 rounded-lg">
-          <GameDisplay/>
-        </div>
-        <div className="flex flex-col w-4/12 h-full ml-6">
-          <div className="w-full h-[10%] rounded-t-lg flex space-x-4">
-            <div
-                className={`w-1/4 h-full rounded-t-lg flex items-center justify-center cursor-pointer ${selectedTab === 'code' ? 'bg-green opacity-90' : 'bg-green opacity-10'}`}
-                onClick={() => setSelectedTab('code')}
-            >
-              <CodeIcon style={{ width: '3rem', height: '3rem', color: 'white' }} />
-            </div>
-            <div
-                className={`w-1/4 h-full rounded-t-lg flex items-center justify-center cursor-pointer ${selectedTab === 'character' ? 'bg-green opacity-90' : 'bg-green opacity-10'}`}
-                onClick={() => setSelectedTab('character')}
-            >
-              <CharacterIcon style={{ width: '3rem', height: '3rem', color: 'white' }} />
-            </div>
-            <div
-                className={`w-1/4 h-full rounded-t-lg flex items-center justify-center cursor-pointer ${selectedTab === 'script' ? 'bg-green opacity-90' : 'bg-green opacity-10'}`}
-                onClick={() => setSelectedTab('script')}
-            >
-              <ScriptIcon style={{ width: '3rem', height: '3rem', color: 'white' }} />
-            </div>
-            <div
-                className={`w-1/4 h-full rounded-t-lg flex items-center justify-center cursor-pointer ${selectedTab === 'local' ? 'bg-green opacity-90' : 'bg-green opacity-10'}`}
-                onClick={() => setSelectedTab('local')}
-            >
-              <LocalIcon style={{ width: '3rem', height: '3rem', color: 'white' }} />
-            </div>
+      <div className="flex flex-col w-full h-full pt-20 space-y-6">
+        <div className="flex items-center justify-between w-full h-[90%]">
+          <div className="w-8/12 h-full bg-transparent border-4 border-green opacity-90 rounded-lg">
+            <GameDisplay />
           </div>
-          <div className="w-full h-[90%] bg-white border-green border-4 border-y-2 opacity-90 rounded-b-lg p-4">
-            {renderContent()}
+          <div className="flex flex-col w-4/12 h-full ml-6">
+            <div className="w-full h-[10%] rounded-t-lg flex space-x-4">
+              <div
+                  className={`w-1/4 h-full rounded-t-lg flex items-center justify-center cursor-pointer ${selectedTab === 'code' ? 'bg-green opacity-90' : 'bg-green opacity-10'}`}
+                  onClick={() => setSelectedTab('code')}
+              >
+                <CodeIcon style={{ width: '3rem', height: '3rem', color: 'white' }} />
+              </div>
+              <div
+                  className={`w-1/4 h-full rounded-t-lg flex items-center justify-center cursor-pointer ${selectedTab === 'character' ? 'bg-green opacity-90' : 'bg-green opacity-10'}`}
+                  onClick={() => setSelectedTab('character')}
+              >
+                <CharacterIcon style={{ width: '3rem', height: '3rem', color: 'white' }} />
+              </div>
+              <div
+                  className={`w-1/4 h-full rounded-t-lg flex items-center justify-center cursor-pointer ${selectedTab === 'script' ? 'bg-green opacity-90' : 'bg-green opacity-10'}`}
+                  onClick={() => setSelectedTab('script')}
+              >
+                <ScriptIcon style={{ width: '3rem', height: '3rem', color: 'white' }} />
+              </div>
+              <div
+                  className={`w-1/4 h-full rounded-t-lg flex items-center justify-center cursor-pointer ${selectedTab === 'local' ? 'bg-green opacity-90' : 'bg-green opacity-10'}`}
+                  onClick={() => setSelectedTab('local')}
+              >
+                <LocalIcon style={{ width: '3rem', height: '3rem', color: 'white' }} />
+              </div>
+            </div>
+            <div className="w-full h-[90%] bg-white border-green border-4 border-y-2 opacity-90 rounded-b-lg p-4">
+              {renderContent()}
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
